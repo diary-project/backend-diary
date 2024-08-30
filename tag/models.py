bfafe38687df8135865f567fd4751e7dd5211fff
Dae-Jeong <v4chelsea@gmail.com>
2024-08-30 15:33:47 +0900

    FIX: Tag 속성 미변경 이슈 해결

--- tag/models.py
+++ tag/models.py
@@ -5,7 +5,7 @@ from diary.models import Diary
 
 class Tag(BaseModel):
     word = models.CharField(max_length=20)
-    diary = models.OneToOneField(Diary, on_delete=models.CASCADE, null=False)
+    diary = models.ForeignKey(Diary, on_delete=models.CASCADE, null=False, related_name='tags')
 
     def __str__(self):
         return self.word
